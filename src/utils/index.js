95e132fca9f5d0bbaa6ba741b9db888c3613a23a
masud040 <masudinfo962@gmail.com>
2024-03-04 14:59:07 +0600

    add utiity fuction for format date

--- src/utils/index.js
+++ src/utils/index.js
@@ -6,6 +6,13 @@ export const formatTime = (formatTime) => {
   let hourDiff = Math.floor(difference / 60);
   difference -= hourDiff * 60;
   let minuteDiff = Math.floor(difference / 1);
-  console.log(minuteDiff);
   difference -= minuteDiff * 1;
+  let message;
+  if (dayDiff > 0) {
+    message = `${dayDiff} days`;
+  }
+  if (hourDiff > 0) {
+    message = message ? `${message} ${hourDiff} hours` : `${hourDiff} hours`;
+  }
+  console.log(message);
 };
